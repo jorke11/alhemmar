cff1b0d9b0eaa6d9aba889da0fb0d926d1283b9c
jorge debian <jpinedom@hotmail.com>
2017-11-02 08:00:51 -0500

    add function scroll

--- public/js/Page/page.js
+++ public/js/Page/page.js
@@ -2,13 +2,13 @@ $(window).scroll(function () {
     var scroll = $(window).scrollTop();
 
     if (scroll > 50) {
-        $(".navbar-default").addClass("menu-black");
-        $(".navbar-default .navbar-nav>li>a").addClass("menu-black-word")
-        $(".navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a").addClass("menu-black-active");
+        $(".navbar-default").addClass("menu-black").fadeIn(16000);
+        $(".navbar-default .navbar-nav>li>a").addClass("menu-black-word").fadeIn(16000);
+        $(".navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a").addClass("menu-black-active").fadeIn(16000);
     } else {
-        $(".navbar-default").removeClass("menu-black");
-        $(".navbar-default .navbar-nav>li>a").removeClass("menu-black-word")
-        $(".navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a").removeClass("menu-black-active");
+        $(".navbar-default").removeClass("menu-black").fadeOut(16000);
+        $(".navbar-default .navbar-nav>li>a").removeClass("menu-black-word").fadeOut(16000)
+        $(".navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a").removeClass("menu-black-active").fadeOut(16000);
     }
 });
 
