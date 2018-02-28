38ace3e9388342d57c864d73161662fb8fe3625b
jorge pinedo <jpinedom@hotmail.com>
2018-02-28 17:55:21 -0500

    add

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -840,7 +840,7 @@
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <p style="color:white;font-size: 18px;">Redes Sociales</p>
+                        <p style="color:white;font-size: 18px;">Siguenos en nuestras Redes Sociales</p>
                     </div>
                 </div>
                 <div class="row">
