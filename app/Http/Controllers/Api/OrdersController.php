1b53263e89b24131109bd53f3372af09c56d28d0
jorge pinedo <jpinedom@hotmail.com>
2018-03-22 13:43:26 -0400

    add

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -62,7 +62,7 @@ class OrdersController extends Controller {
 
             if ($result) {
 
-                $param = (array) DB::table("vorders")->where("id", $result->id)->first();
+                $in = (array) DB::table("vorders")->where("id", $result->id)->first();
                 $sche = Schedules::find($in["schema_id"]);
                 $in["schema"] = $sche->description;
                 $in["schema_detail"] = SchedulesDetail::select("schedules_detail.id", "courses.description as course")
